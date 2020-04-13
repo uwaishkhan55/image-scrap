7dc9e17faa5313cfdb9971032fe6ddf8495482f6
uwaish55 <uwaishkhan55@gmail.com>
2020-04-13 15:32:12 +0530

    @v1.0.1

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var Scraper = require('images-scraper');
-
+const Scraper=require('./scrap')
  //change product name according to your need
  //we can also add country name along with productname for more accurate results.
 let productName="kulfi";
 
 //No of images you want 
-let noOfImages=20;
+let noOfImages=5;
 const google = new Scraper({
   puppeteer: {
       //if yout want to check what is happening behind the script then just change the value of headless -> false.
-    headless: true,
+    headless: false,
   }
 });
  
@@ -17,6 +16,6 @@ const google = new Scraper({
   const results = await google.scrape(productName, noOfImages);
   let i=1;
   results.forEach(imgUrl=>{
-      console.log(i++ +" "+imgUrl.url)
+      console.log(i++ +". "+imgUrl.url)
   })
 })();
